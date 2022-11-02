86fa2f85dec883bde882e7041eedf400f0ec3abf
sarabojo <sarabojo@gmail.com>
2022-11-02 19:00:22 -0400

    try to add key and mapping ratings in dogs component.

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -5,6 +5,13 @@ const Dogs = (props) => {
       <h1>Dogs Component</h1>
       <li>
         <img src={props.data.photo} alt={props.data.breed_name} />
+        <h3>Dog Breed: {props.data.breed_name} </h3>
+        <h5>Breed Description: {props.data.description}</h5>
+        <ul>
+          {props.Ratings.map((rating) => (
+            <Ratings key={data.id} rating={rating} />
+          ))}
+        </ul>
       </li>
     </div>
   )
--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -3,6 +3,9 @@ const Ratings = (props) => {
     <li>
       <h1>Ratings Section</h1>
       <h5>{props.data.saras_rating}</h5>
+      <h5>Is the dog friendly? {props.data.friendly}</h5>
+      <h5>Does it shed a lot? {props.data.sheds}</h5>
+      <h5>Is the dog easy to train? {props.data.easy_to_train}</h5>
     </li>
   )
 }
