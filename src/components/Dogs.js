84f75e4d2e66a0bd607203c14ed99fd32b9594bc
sarabojo <sarabojo@gmail.com>
2022-11-04 00:01:47 -0400

    moved breed up top, got better padding and margins for the boxeas.

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -2,9 +2,8 @@ import Ratings from './Ratings'
 const Dogs = (props) => {
   return (
     <div className="dog-info">
-      <h1></h1>
+      <h1> {props.breed_name}</h1>
       <img src={props.photo} alt={props.breed_name} />
-      <h3>Dog Breed: {props.breed_name} </h3>
       <h3> Description: {props.description} </h3>
       <Ratings rating={props.saras_rating} />
     </div>
